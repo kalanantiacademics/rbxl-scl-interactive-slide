48e2d4351e2ef0c428691e062c7280e5adf83a85
mandashaumy <amandyshaumy@gmail.com>
2026-03-04 12:06:03 +0700

    update basename and vite config for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import LessonView from "./materials/LessonView.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Day1 from "./materials/Day1.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rbxl-scl-interactive-slide">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/lesson/RBXL_1" element={<Day1 />} />
